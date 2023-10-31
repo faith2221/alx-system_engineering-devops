84864a4f9c9992951cbafb83fda53a29ed254d5a
faith2221 <edithbanda014@gmail.com>
2023-10-31 10:36:17 -0700

    Mandatory and Advanced Regular expression

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,4 @@
-#!/ur/bin/env ruby
+#!/usr/bin/env ruby
 # Ruby script that accepts one argument
 # Passes it to a regular expression matching method
 puts ARGV[0].scan(/School/).join
--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,3 +1,3 @@
-#!/ur/bin/env ruby
+#!/usr/bin/env ruby
 # Regular expression that matches a given pattern
 puts ARGV[0].scan(/[A-Z]/).join
