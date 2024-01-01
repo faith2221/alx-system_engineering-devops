b57beed18845248bf6b4efa1d724713ea153c4cc
faith2221 <edithbanda014@gmail.com>
2023-12-31 23:59:20 -0800

    Correction to Advanced Task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Configuring ssh config file
 
-file_line{'Turn off passwd auth':
-  path => '/etc/ssh/ssh_config',
-  line => '    PasswordAuthentication no',
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
 
-file_line{'Declare identity file':
-  path => '/etc/ssh/ssh_config',
-  line => '    IdentifyFile ~/.ssh/school',
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentifyFile ~/.ssh/school',
 }
