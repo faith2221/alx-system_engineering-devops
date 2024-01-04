9bac9f6361961c79c35ab50e0cb1d65e18b267e1
faith2221 <edithbanda014@gmail.com>
2024-01-03 18:28:47 -0800

    Edits

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,25 +1,21 @@
 # Installs nginx using puppet
 
-exec { 'apt-get-update':
-  command  => '/usr/bin/apt-get update',
+package { 'nginx':
+  ensure  => installed,
 }
 
-package { 'nginx':
-  ensure   => installed
-  require  => Exec['apt-get-update'],
+file_line { 'server_config':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-  content  => 'Hello World!',
-  require  => Package['nginx'],
-}
-
-exec { 'redirect-me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell'
+  content => 'Hello World!',
 }
 
 service { 'nginx':
-  ensure   => running,
-  require  => Package['nginx'],
+  ensre   => running,
+  require => Package['nginx'],
 }
