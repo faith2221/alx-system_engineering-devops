efc233819df1f5e5d481e9c47e8082b41e5745a7
faith2221 <edithbanda014@gmail.com>
2024-01-28 22:55:43 -0800

    0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Gathers the data feom an API."""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+    base_data = "https://jsonplaceholder.typicode.com/users"
+    url = base_data + "/" + employee_id
+
+    user_response = requests.get(url)
+    employee_name = response.json().get('name')
+
+    todo_data = url + "/todos"
+    user_response = requests.get(todo_data)
+    tasks = response.json()
+    total_tasks = 0
+    completed_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            completed_tasks.append(task)
+            total_tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, total_tasks, len(tasks)))
+
+    for task in completed_tasks:
+        print("\t {}".format(task.get('title')))
