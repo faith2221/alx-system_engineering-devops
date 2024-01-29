0a5e45730a15661c5c0e9800361894c9b34ac1e6
faith2221 <edithbanda014@gmail.com>
2024-01-28 23:30:08 -0800

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/users"
 
-    user_response = requests.get(url)
+    response = requests.get(url)
     users = response.json()
 
     dictionary = {}
@@ -18,12 +18,12 @@ if __name__ == '__main__':
         username = user.get('username')
         url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
         url = url + '/todos/'
-        user_response = requests.get(url)
+        response = requests.get(url)
         tasks = response.json()
         dictionary[user_id] = []
         for task in tasks:
             dictionary[user_id].append({
-                "task": task.et('title'),
+                "task": task.get('title'),
                 "completed": task.get('completed'),
                 "username": username
                 })
