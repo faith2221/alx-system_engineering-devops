4ac5eb610a2f2070496e62ce94368cc3fc0375cf
faith2221 <edithbanda014@gmail.com>
2024-01-28 23:11:32 -0800

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Gathers the data fron an API and exports it to CSV."""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+    base_data = "https://jsonplaceholder.typicode.com/users"
+    url = base_data + "/" + employee_id
+
+    user_response = requests.get(url)
+    username = response.json().get('username')
+
+    todo_data = url + "/todos"
+    user_response = requests.get(todo_data)
+    tasks = response.json()
+
+    dictionary = {employee_id: []}
+    for task in tasks:
+        dictionary[employee_id].append({
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": username
+            })
+    with open('{}.json'.format(employee_id), 'w') as filename:
+        json.dump(dictionary, filename)
