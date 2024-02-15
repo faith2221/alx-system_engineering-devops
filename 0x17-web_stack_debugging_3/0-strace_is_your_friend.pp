6f314038c3f7c8d521be63a0c24c35942e5fd063
faith2221 <edithbanda014@gmail.com>
2024-02-15 06:37:20 -0800

    Corrections

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -5,4 +5,5 @@ $file_to_edit = '/var/www/html/wp-settings.php'
 # Replace line containing "phpp" with "php"
 exec { 'replace_line':
   command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin', '/usr/bin']
+  path    => ['/bin', '/usr/bin'],
+}
