6a565b681b0a85912980481c0330fa46935b8199
faith2221 <121094120+faith2221@users.noreply.github.com>
2024-03-02 07:22:59 +0200

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,9 +0,0 @@
-# Puppet manifest to replace a line in a file on a server.
-
-$file_to_edit = '/var/www/html/wp-settings.php'
-
-# Replace line containing "phpp" with "php"
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin', '/usr/bin'],
-}
