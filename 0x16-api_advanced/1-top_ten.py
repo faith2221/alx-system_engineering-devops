ecbc5f88e6ca9b6af7e333fa3ba1a6e684dc849e
faith2221 <edithbanda014@gmail.com>
2024-03-05 02:27:23 -0800

    Additions to Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,15 +10,26 @@ import requests
 def top_ten(subreddit):
     """Prints the10 hottest titles post on a given subreddit."""
     # Construct the URL subreddits host posts in JSON format
-    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
 
     # Defines headers for the HTTP request including User-Agent
     headers = {
             "User-Agent": "Google Chrome Version 122.0.6261.95"
             }
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        results = response.json()['data']['children']
-        [print(post['data']['title']) for post in results]
-    else:
-        print(None)
+    # Defines parameters for the request, limiting number of posts to 10
+    params = {"limit": 10}
+
+    # Sends a GET request to the subreddit's hot posts page
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    # Checks if response status code indicates a not-found error(404)
+    if response.status_code == 404:
+        PRINT("None")
+        return
+
+    # Parse the JSON response and extract the 'data' section
+    results = response.json()['data']['children']
+
+    # Print the titles of the top 10 hottest posts
+    [print(c.get("data").get("title")) for c in results.get("children")]
