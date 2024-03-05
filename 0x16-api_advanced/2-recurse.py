53e500b72ee6383abd1bf3ff53cc41c35eb33050
faith2221 <edithbanda014@gmail.com>
2024-03-05 01:46:06 -0800

    Corrections

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,20 +4,23 @@ Lists 10 hot psts under given subreddit Using recursion
 """
 
 import requests
+after = None
 
 
 def recurse(subreddit, hot_list=[]):
     """If no results found, returns None."""
-    user = {"User-Agent": "0x16.api_advanced"}
+    global after
+    user_agent = {"User-Agent": "api_advanced-project"}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=user,
+    results = requests.get(url, params=parameters, headers=user_agent,
                            allow_redirects=False)
 
     if results.status_code == 200:
         after_data = results.json().get("data").get("after")
         if after_data is not None:
-            recurse(subreddit, after_data, hot_list)
+            after = after_data
+            recurse(subreddit, hot_list)
         all_titles = results.json().get("data").get("children")
         for title_ in all_titles:
             hot_list.append(title_.get("data").get("title"))
