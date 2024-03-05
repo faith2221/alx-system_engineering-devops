e5840e2aababdd4f4a85986f049b81d386654ed6
faith2221 <edithbanda014@gmail.com>
2024-03-05 01:55:47 -0800

    Corrections to Advanced Task

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -30,7 +30,7 @@ def count_words(subreddit, word_list, after="", count=[]):
         after = data['data']['after']
         if after is None:
             save = []
-            for i in range(len(word-list)):
+            for i in range(len(word_list)):
                 for j in range(i + 1, len(word_list)):
                     if word_list[i].lower() == word_list[j].lower():
                         save.append(j)
