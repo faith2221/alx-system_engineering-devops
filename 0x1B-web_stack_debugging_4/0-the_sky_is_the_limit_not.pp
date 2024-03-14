91db01d0cc7bc5e67f80989c2a9faab974320b6a
faith2221 <edithbanda014@gmail.com>
2024-03-14 00:29:15 -0700

    corrections to Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,7 +3,7 @@
 #Increase ULIMIT nuumber
 exec { 'fix-for-nginx':
 command => 'sed -i  "s/15/4096/" /etc/default/nginx',
-path	=> '/usr/local/bin/:/bin',
+path	=> '/usr/local/bin:/bin',
 }
 
 # Restarting Nginx
